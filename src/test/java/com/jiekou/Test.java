1ac2b1defa5d4c5a5155b82abdbceae8b458b4a2
John Doe <john@sample.com>
2018-10-25 16:39:48 +0800

    测试修改内容

--- src/test/java/com/jiekou/Test.java
+++ src/test/java/com/jiekou/Test.java
@@ -6,6 +6,6 @@ package com.jiekou;
 public class Test {
     @org.testng.annotations.Test
     public void http(){
-        JIEKOU.test("http://baidu.com");
+        JIEKOU.test("http://route.showapi.com/341-2?showapi_appid=78127&showapi_sign=32c114d1961f5fcab45bf417d3930314&page=2");
     }
 }
