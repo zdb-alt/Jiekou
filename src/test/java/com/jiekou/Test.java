880036f7466c221674ce8e0d01d2db5cc60606e3
John Doe <john@sample.com>
2018-10-25 15:47:18 +0800

    Initial commit

---
+++ src/test/java/com/jiekou/Test.java
@@ -0,0 +1,11 @@
+package com.jiekou;
+
+/**
+ * Created by Administrator on 2018/10/25.
+ */
+public class Test {
+    @org.testng.annotations.Test
+    public void http(){
+        JIEKOU.test("http://baidu.com");
+    }
+}
