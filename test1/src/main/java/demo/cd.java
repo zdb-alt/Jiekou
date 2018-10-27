73191686e9fb26e2820ba0fcf8edf01520e88a2d
MinhongShen <761591766@qq.com>
2018-10-27 18:29:43 +0800

    删除代码，测试报告部分还是没有完成

---
+++ test1/src/main/java/demo/cd.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Assert;
+
+/**
+ * Created by XULE on 2018/10/27.
+ */
+public class cd {
+    @org.testng.annotations.Test
+    public  void t1(){
+        Assert.assertEquals(1,7);
+    }
+
+}
