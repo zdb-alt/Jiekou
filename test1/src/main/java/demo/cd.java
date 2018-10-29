8488b060dfab8cb008b6b3fee25925db4f4f3433
John Doe <john@sample.com>
2018-10-29 18:15:58 +0800

    修改监听类

--- test1/src/main/java/demo/cd.java
+++ test1/src/main/java/demo/cd.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  * Created by XULE on 2018/10/27.
@@ -8,7 +9,15 @@ import org.testng.Assert;
 public class cd {
     @org.testng.annotations.Test
     public  void t1(){
+
         Assert.assertEquals(1,7);
     }
+    @Test
+    public void t2(){
+        Assert.assertEquals(1,1);
+    }
+
+
+
 
 }
